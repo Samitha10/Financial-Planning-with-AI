ca1bc798376732b1aa05856a750ef5bb4dfebbb4
Samitha10 <samithabesinghe1999@gmail.com>
2024-02-10 18:07:01 +0530

    Add new Python compiled files for Backend routes and Charts

---
+++ Backend/main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI, HTTPException,APIRouter
+import pymongo, traceback
+from pymongo.mongo_client import MongoClient
+import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
+
+from routes.Automate import AutomateRoute
+from routes.froute import froute
+from routes.Charts.valueCounts import valueCountsRoute    
+from routes.Charts.Sales import SalesRoute 
+
+app = FastAPI()
+app.include_router(froute, prefix="/Froute", tags=["Froute"])
+app.include_router(AutomateRoute, prefix="/Automate", tags=["Automate"])
+app.include_router(valueCountsRoute, prefix="/ValueCounts", tags=["Froute - ValueCounts"])
+app.include_router(SalesRoute, prefix="/Sales", tags=["Froute - Sales"])
+
+origins = [
+    "http://localhost:3000",  # React app address
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/')
+def one():
+    return {'This is first api':'Hello World'}
+
+{# url = "mongodb+srv://shanukafer98:Mongodb123.@cluster0.gtbdj6v.mongodb.net/?retryWrites=true&w=majority"  
+# client = MongoClient(url)
+# db = client.SSD
+# collection = db.Super_Store_Data
+
+# @app.get('/getdata')
+# def getdata():
+#     try:
+#         result = list(collection.find())
+#         # Convert ObjectId to str for serialization
+#         for doc in result:
+#             if "_id" in doc:
+#                 doc["_id"] = str(doc["_id"])
+#         df = pd.DataFrame(result)
+#         # Drop the '_id' column from the DataFrame
+#         df = df.drop("_id", axis=1)
+
+#     except Exception as e:
+#         print(f"Exception: {str(e)}")
+#         traceback.print_exc()
+#         raise HTTPException(status_code=500, detail="Internal Server Error")
+    
+#     df.to_csv('fullData.csv',index=True)
+
+#     # Convert the modified DataFrame to a dictionary
+#     result1 = df.to_dict(orient='records')
+
+#     return {"data": result1}
+}
+
---
+++ Backend/routes/Charts/valueCounts.py
@@ -0,0 +1,131 @@
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+from typing import Dict
+import json
+import pymongo
+from functools import lru_cache
+
+valueCountsRoute = APIRouter()
+
+MONGO_URI = "mongodb+srv://shanukafer98:Mongodb123.@cluster0.gtbdj6v.mongodb.net/SSD"
+COLLECTION_NAME = "Charts"
+client = pymongo.MongoClient(MONGO_URI)
+db = client.get_database()
+collection = db[COLLECTION_NAME]
+
+@valueCountsRoute.get("/valueCounts_shipMode_Frontend")
+@lru_cache(maxsize=32)
+def get_shipmode_counts():
+    try:
+        docs = collection.find()
+        for doc in docs:
+            if 'ship_mode_counts' in doc:
+                ship_mode_counts = json.loads(doc['ship_mode_counts'])
+                return JSONResponse(content=ship_mode_counts)
+        return {"message": "No data found"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@valueCountsRoute.get("/valueCounts_segment_Frontend")
+@lru_cache(maxsize=32)
+def get_segment_counts():
+    try:
+        docs = collection.find()
+        for doc in docs:
+            if 'segment_counts' in doc:
+                segment_counts = json.loads(doc['segment_counts'])
+                return JSONResponse(content=segment_counts)
+        return {"message": "No data found"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@valueCountsRoute.get("/valueCounts_state_Frontend")
+@lru_cache(maxsize=32)
+def get_state_counts():
+    try:
+        docs = collection.find()
+        for doc in docs:
+            if 'state_counts' in doc:
+                state_counts = json.loads(doc['state_counts'])
+                return JSONResponse(content=state_counts)
+        return {"message": "No data found"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@valueCountsRoute.get("/valueCounts_country_Frontend")
+@lru_cache(maxsize=32)
+def get_country_counts():
+    try:
+        docs = collection.find()
+        for doc in docs:
+            if 'country_counts' in doc:
+                country_counts = json.loads(doc['country_counts'])
+                return JSONResponse(content=country_counts)
+        return {"message": "No data found"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@valueCountsRoute.get("/valueCounts_market_Frontend")
+@lru_cache(maxsize=32)
+def get_market_counts():
+    try:
+        docs = collection.find()
+        for doc in docs:
+            if 'market_counts' in doc:
+                market_counts = json.loads(doc['market_counts'])
+                return JSONResponse(content=market_counts)
+        return {"message": "No data found"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@valueCountsRoute.get("/valueCounts_category_Frontend")
+@lru_cache(maxsize=32)
+def get_category_counts():
+    try:
+        docs = collection.find()
+        for doc in docs:
+            if 'category_counts' in doc:
+                category_counts = json.loads(doc['category_counts'])
+                return JSONResponse(content=category_counts)
+        return {"message": "No data found"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@valueCountsRoute.get("/valueCounts_region_Frontend")
+@lru_cache(maxsize=32)
+def get_region_counts():
+    try:
+        docs = collection.find()
+        for doc in docs:
+            if 'region_counts' in doc:
+                region_counts = json.loads(doc['region_counts'])
+                return JSONResponse(content=region_counts)
+        return {"message": "No data found"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@valueCountsRoute.get("/valueCounts_subCategory_Frontend")
+@lru_cache(maxsize=32)
+def get_subCategory_counts():
+    try:
+        docs = collection.find()
+        for doc in docs:
+            if 'sub_category_counts' in doc:
+                sub_category_counts = json.loads(doc['sub_category_counts'])
+                return JSONResponse(content=sub_category_counts)
+        return {"message": "No data found"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@valueCountsRoute.get("/valueCounts_orderPriority_Frontend")
+@lru_cache(maxsize=32)
+def get_orderPriority_counts():
+    try:
+        docs = collection.find()
+        for doc in docs:
+            if 'order_priority_counts' in doc:
+                order_priority_counts = json.loads(doc['order_priority_counts'])
+                return JSONResponse(content=order_priority_counts)
+        return {"message": "No data found"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
